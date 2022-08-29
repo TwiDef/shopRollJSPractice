edffb23f918bee729320a27e7575f4a16a654429
Def <deferenseo@gmail.com>
2022-08-29 17:30:25 +0300

    modified counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,14 +1,16 @@
 window.addEventListener('click', (event) => {
+    const counterWrapper = event.target.closest('.counter-wrapper'); // нахождение родителя
+    const counter = counterWrapper.querySelector('[data-counter]');
+
     if (event.target.dataset.action === 'plus') {
-        const counterWrapper = event.target.closest('.counter-wrapper'); // нахождение родителя
-        const counter = counterWrapper.querySelector('[data-counter]');
         counter.innerText = ++counter.innerText;
     }
+
     if (event.target.dataset.action === 'minus') {
-        const counterWrapper = event.target.closest('.counter-wrapper'); // нахождение родителя
-        const counter = counterWrapper.querySelector('[data-counter]');
         if (parseInt(counter.innerText) > 1) {
             counter.innerText = --counter.innerText;
+        } else if (event.target.closest('.cart-wrapper') && parseInt(counter.innerText) === 1) {
+            event.target.closest('.cart-item').remove();
         }
     }
 });
